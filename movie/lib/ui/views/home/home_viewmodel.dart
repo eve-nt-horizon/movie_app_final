6bb31718e64ae617da69353f45546fc3adb90608
mikaflatah@gmail.com <Mika>
2024-06-01 14:26:41 +0530

    dispose and getters

--- movie/lib/ui/views/home/home_viewmodel.dart
+++ movie/lib/ui/views/home/home_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:movie/app/app.locator.dart';
 import 'package:movie/models/tmdb/tmdb_movie_basic.dart';
 import 'package:movie/services/api_service.dart';
@@ -8,15 +9,22 @@ import 'package:stacked/stacked.dart';
 class HomeViewModel extends BaseViewModel {
   final _apiService = locator<ApiService>();
   final _movieCarouselService = locator<MovieCarouselService>();
-  TextEditingController searchController = TextEditingController();
+  final TextEditingController _searchController = TextEditingController();
+  TextEditingController get searchController => _searchController;
   Future search() async {
-    if (searchController.text.isEmpty) return;
-    List<TMDBMovieBasic> res = await _apiService.search(searchController.text);
+    if (_searchController.text.isEmpty) return;
+    List<TMDBMovieBasic> res = await _apiService.search(_searchController.text);
     print(res);
   }
 
   Future runStartupLogic() async {
     List<TMDBMovieBasic> movies = await _apiService.popularMovies();
     _movieCarouselService.movieList.assignAll(movies);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _searchController.dispose();
   }
 }
--- movie/lib/ui/widgets/common/movie_carousel/movie_carousel_model.dart
+++ movie/lib/ui/widgets/common/movie_carousel/movie_carousel_model.dart
@@ -3,26 +3,26 @@ import 'package:movie/app/app.locator.dart';
 import 'package:movie/models/tmdb/tmdb_movie_basic.dart';
 import 'package:movie/services/movie_carousel_service.dart';
 import 'package:stacked/stacked.dart';
-import 'package:movie/movie.dart';
+
 
 final _movieCarouselService = locator<MovieCarouselService>();
 
 class MovieCarouselModel extends StreamViewModel {
-  ReactiveList<TMDBMovieBasic> get movieList => _movieCarouselService.movieList;
+  int currentPage = 1;
+  late PageController _pageController;
 
   @override
   Stream get stream => _movieCarouselService.movieList.onChange;
-  int currentPage = 1;
-  late PageController pageController;
+  ReactiveList<TMDBMovieBasic> get movieList => _movieCarouselService.movieList;
+  PageController get pageController => _pageController;
 
   MovieCarouselModel() {
-    pageController = PageController(
+    _pageController = PageController(
       viewportFraction: 0.8,
       initialPage: currentPage,
     );
   }
 
-  get movies => _movies;
   void setCurrentPage(int value) {
     currentPage = value;
     rebuildUi();
@@ -34,116 +34,10 @@ class MovieCarouselModel extends StreamViewModel {
     super.onData(data);
     rebuildUi();
   }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
 }
-
-// our demo data movie data
-List<Movie> _movies = [
-  Movie(
-    id: 1,
-    title: "Bloodshot",
-    year: 2020,
-    poster: "assets/images/poster_1.jpg",
-    backdrop: "assets/images/backdrop_1.jpg",
-    numOfRatings: 150212,
-    rating: 7.3,
-    criticsReview: 50,
-    metascoreRating: 76,
-    genra: ["Action", "Drama"],
-    plot: plotText,
-    cast: [
-      {
-        "orginalName": "James Mangold",
-        "movieName": "Director",
-        "image": "assets/images/actor_1.png",
-      },
-      {
-        "orginalName": "Matt Damon",
-        "movieName": "Carroll",
-        "image": "assets/images/actor_2.png",
-      },
-      {
-        "orginalName": "Christian Bale",
-        "movieName": "Ken Miles",
-        "image": "assets/images/actor_3.png",
-      },
-      {
-        "orginalName": "Caitriona Balfe",
-        "movieName": "Mollie",
-        "image": "assets/images/actor_4.png",
-      },
-    ],
-  ),
-  Movie(
-    id: 2,
-    title: "Ford v Ferrari ",
-    year: 2019,
-    poster: "assets/images/poster_2.jpg",
-    backdrop: "assets/images/backdrop_2.jpg",
-    numOfRatings: 150212,
-    rating: 8.2,
-    criticsReview: 50,
-    metascoreRating: 76,
-    genra: ["Action", "Biography", "Drama"],
-    plot: plotText,
-    cast: [
-      {
-        "orginalName": "James Mangold",
-        "movieName": "Director",
-        "image": "assets/images/actor_1.png",
-      },
-      {
-        "orginalName": "Matt Damon",
-        "movieName": "Carroll",
-        "image": "assets/images/actor_2.png",
-      },
-      {
-        "orginalName": "Christian Bale",
-        "movieName": "Ken Miles",
-        "image": "assets/images/actor_3.png",
-      },
-      {
-        "orginalName": "Caitriona Balfe",
-        "movieName": "Mollie",
-        "image": "assets/images/actor_4.png",
-      },
-    ],
-  ),
-  Movie(
-    id: 1,
-    title: "Onward",
-    year: 2020,
-    poster: "assets/images/poster_3.jpg",
-    backdrop: "assets/images/backdrop_3.jpg",
-    numOfRatings: 150212,
-    rating: 7.6,
-    criticsReview: 50,
-    metascoreRating: 79,
-    genra: ["Action", "Drama"],
-    plot: plotText,
-    cast: [
-      {
-        "orginalName": "James Mangold",
-        "movieName": "Director",
-        "image": "assets/images/actor_1.png",
-      },
-      {
-        "orginalName": "Matt Damon",
-        "movieName": "Carroll",
-        "image": "assets/images/actor_2.png",
-      },
-      {
-        "orginalName": "Christian Bale",
-        "movieName": "Ken Miles",
-        "image": "assets/images/actor_3.png",
-      },
-      {
-        "orginalName": "Caitriona Balfe",
-        "movieName": "Mollie",
-        "image": "assets/images/actor_4.png",
-      },
-    ],
-  ),
-];
-
-String plotText =
-    "American car designer Carroll Shelby and driver Kn Miles battle corporate interference and the laws of physics to build a revolutionary race car for Ford in order.";
